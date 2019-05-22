044c809d474c0ac707d8f73c1864969d8926a5bf
ninpnin <vaino.yrjanainen@aalto.fi>
2019-05-22 22:29:04 +0300

    Fix dataset syllablizer structure

--- syllable-level/preprocessing/dataset_syllablizer.py
+++ syllable-level/preprocessing/dataset_syllablizer.py
@@ -5,11 +5,16 @@ def convert_to_syllables(data_filename, vocab_filename, out_filename):
 	print("Done!")
 	return []
 
+def write_processed_data(syllables, filename):
+	print("Write preprocessed data...")
+	print("Done!")
+
 def main():
 	data_file = "data.txt"
 	vocab_file = "vocab.txt"
 	out_file = "data_preprocessed.txt"
-	convert_to_syllables(data_file, vocab_file, out_file)
+	syllables = convert_to_syllables(data_file, vocab_file)
+	write_processed_data(syllables, out_file)
 
 if __name__ == "__main__":
 	main()
