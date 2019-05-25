4377db1bae6841a7bc78f336c4c0bf6387981a9e
ninpnin <vaino.yrjanainen@aalto.fi>
2019-05-25 11:55:40 +0300

    Expand preprocessing code

--- syllable-level/preprocessing/dataset_syllablizer.py
+++ syllable-level/preprocessing/dataset_syllablizer.py
@@ -5,6 +5,23 @@ def convert_to_syllables(data_filename, vocab_filename, out_filename):
 	print("Done!")
 	return []
 
+def convert_to_indices(syllables):
+	# the model will take as input an integer matrix
+	# of size (batch, input_length). The integers range
+	# from 0 to syllable set size
+
+	# This function converts dataset of syllables
+	# into a list of syllable indices
+
+	# The input matrix X is a list of lists of syllable indices
+	# X := [[1,3,2,0,0,3], ... , [5,5,6,0,1,0]]
+	# Size: (batch_size, input_length)
+
+	# The output/labels matrix Y is is list of syllables
+	# Y := [6,7,3, ... ,0,12,2]
+	# Size: (batch_size)
+	return []
+
 def write_processed_data(syllables, filename):
 	print("Write preprocessed data...")
 	print("Done!")
--- syllable-level/preprocessing/vocab_builder.py
+++ syllable-level/preprocessing/vocab_builder.py
@@ -17,15 +17,32 @@ def naive_syl_search(s):
 
 	d = syllables.items()
 	d = sorted(d, key=lambda x: x[1], reverse=True)
-	print(d[:5000])
+	return d[:5000]
 
+def syllable_dijkstra(word, syls):
+	available_syls = []
+	l = len(word)
+	for i in range(0, l):
+		for j in range(i+1, l+1):
+			substring = word[i:j]
+			print(substring)
+			if substring not in available_syls and substring in syls:
+				available_syls.append(substring)
+	print(syls)
+	print(available_syls)
 
 def generate_vocab(in_filename):
 	print("Generate vocab...")
 	print("Done!")
 	f = open("whole_dataset.txt","r").read().lower()
 	f = re.sub("[^a-zäö] ", "", f)
-	naive_syl_search(f)
+	syls = naive_syl_search(f)
+
+	syls2 = []
+	for el, val in syls:
+		syls2.append(el)
+
+	syllable_dijkstra("hegeisevä", syls2)
 	return []
 
 def write_vocab(vocab, out_filename):
